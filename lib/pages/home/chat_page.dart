624b975222ed118ff5dcff4d369a8dce9b3813d2
kevinrisqi <kevinrisqi22@gmail.com>
2022-04-04 12:37:19 +0700

    Create Chat Page Empty

--- lib/pages/home/chat_page.dart
+++ lib/pages/home/chat_page.dart
@@ -6,11 +6,91 @@ class ChatPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Chat Page',
-        style: primaryTextStyle,
+    return Column(
+      children: [
+        header(),
+        Expanded(
+          child: Container(
+            color: bgColor3,
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/image_earphone.png',
+                  width: 80,
+                  height: 80,
+                  fit: BoxFit.cover,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'Opss no message yet?',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: medium,
+                  ),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Text(
+                  'You have never done a transaction',
+                  style: secondaryTextStyle.copyWith(
+                    fontSize: 14,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 24,
+                        vertical: 10,
+                      ),
+                      backgroundColor: primaryColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      )),
+                  child: Text(
+                    'Explore Store',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: medium,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class header extends StatelessWidget {
+  const header({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: bgColor1,
+      title: Text(
+        'Message Support',
+        style: primaryTextStyle.copyWith(
+          fontSize: 18,
+          fontWeight: medium,
+        ),
       ),
+      centerTitle: true,
+      elevation: 0,
+      automaticallyImplyLeading: false,
     );
   }
 }
